e1796ff143fc88161f34040cdcb185c834af0338
WuXingYu <1043397668@qq.com>
2023-10-02 23:10:26 +0800

    共用模块

---
+++ find-link-common/common-result/src/main/java/bean/Constant.java
@@ -0,0 +1,7 @@
+package bean;
+
+public class Constant {
+    public static String SUCCESS = "200";
+    public static String FAIL = "500";
+
+}
---
+++ find-link-common/common-result/src/main/java/bean/Result.java
@@ -0,0 +1,20 @@
+package bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Result<T> {
+    private String code;
+    private String message;
+    private T content;
+
+    public Result(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
+
